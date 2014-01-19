7eff50e66356d94bb8729931721071199cd8c9c1
Benjamin Kovach <bkovach13@gmail.com>
2014-01-19 13:00:23 -0500

    Add walls, ladders, small refactor of Block

--- src/Types/Room.hs
+++ src/Types/Room.hs
@@ -18,24 +18,53 @@ data RoomType = NormalRoom
               | LevelExit
                 deriving (Enum, Eq)
 
+-- | Placeholder
+type Wall = Int
+
 data Room = Room{
     _entities :: [(Space, Entity)],
-    _rType    :: RoomType
+    _rType    :: RoomType,
+    -- | See 1/19 notes
+    -- | Define like this for absolute correctness,
+    -- | Don't allow for an Up wall, for example
+    _ladderU  :: Bool,
+    _ladderD  :: Bool,
+    _wallN    :: Maybe Wall,
+    _wallS    :: Maybe Wall,
+    _wallE    :: Maybe Wall,
+    _wallW    :: Maybe Wall
 }
 
+makeLenses ''Room
+
 instance Show Room where
-    show (Room es r_type) = intercalate "\n" $  (extra: map show' es)
+    show r = intercalate "\n" $  (ladders : t : map show' (r^.entities))
         where show' (spc, entity) = case entity of
                 _         -> "There is a "  ++ show entity ++ " in the " ++ showRelativeDirection spc ++ "."
-              extra = case r_type of
+              t = case (r^.rType) of
                 KaliAltar      -> "You see an altar to Kali."
                 Shop           -> "You have found a shop!"
                 LevelExit      -> "You have found the exit!"
                 _         -> []
+              ladders = case (r^.ladderU, r^.ladderD) of
+                (True, True) -> "There are ladders both up and down."
+                (True, _   ) -> "There is a ladder up."
+                (_   , True) -> "There is a ladder down."
+                _            -> []
+              walls = let ws = map (r^.) [wallN, wallS, wallE, wallW] in
+                      map showWall ws
+                        -- | TODO: Implement
+                        where showWall = undefined
+
 
 instance Universe RoomType where
   universe = enumFrom NormalRoom
   
 --completely void room
 instance Default Room where
-  def = Room [] NormalRoom
+  def = Room 
+          []          -- | Entities
+          NormalRoom  -- | Room Type
+          False False -- | Ladders
+          Nothing Nothing Nothing Nothing -- | Walls
+
