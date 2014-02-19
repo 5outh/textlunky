8a693a93e3da3b84698aaefe5f5feecf29836cc9
5outh <bkovach13@gmail.com>
2014-02-19 17:52:50 -0500

    Qualify Game.hs imports and remove old comments that don't make sense anymore.

--- src/Game.hs
+++ src/Game.hs
@@ -11,45 +11,32 @@ module Game(
 
 where
 
-import Control.Monad.Trans
-import Control.Monad.Trans.Free
-import Control.Monad.Trans.State
-import Types
-import Control.Monad(forever)
-import TextlunkyParser
-import Control.Process
-import Control.Lens hiding ((<.>))
-import Prelude hiding (round)
-
-makeLenses ''GameState
+import           Control.Process
+import qualified Control.Process as P
+import           Control.Monad.Trans
+import           Control.Monad.Trans.Free
+import           Control.Monad.Trans.State
+import           Types
+import           Control.Monad(forever)
+import qualified TextlunkyParser as TP
 
 -- | See below for details
 runGame :: GameState -> IO ()
 runGame gs = flip evalStateT gs $ (runCommand game)
 
--- |  What we wnat to do here is:
--- |  0. Print a description of the room the player is in
--- |  1. Get input from the user (using the IO monad)
--- |  2. Build a Free TextlunkyCommand from it (using the Free monad)
--- |  3. Modify the game based on the above Free monad (Using StateT GameState)
--- |  4. Print out user command (using runCommand)
--- |  5. Step the rest of the game (Modify the game state using 
--- |     a function GameState -> GameState)
--- |  6. Repeat.
+-- | Get and run each step of the game
 game :: Textlunky ()
 game = forever $ (liftIO $ putStr "> ") >> prompt
 
 -- | Build a textlunky command from the command line
 prompt :: Textlunky ()
-prompt = liftIO getLine >>= parseInput
+prompt = liftIO getLine >>= TP.parseInput
 
--- Process command with access to global state
--- | NB. updateP <.> showP should (will?) update and show each command.
--- | These reside in Control.Process
+-- | Process command with access to global state
 runCommand :: Textlunky () -> Global GameState ()
 runCommand t = do
   cmd <- runFreeT t
   case cmd of
     Pure _   -> return ()
     Free End -> liftIO $ putStrLnP $ "Goodbye!"
-    _        -> recursively runCommand (stepGame <.> showP <.> updateP) cmd
+    _        -> P.recursively runCommand (P.stepGame <.> P.showP <.> P.updateP) cmd
