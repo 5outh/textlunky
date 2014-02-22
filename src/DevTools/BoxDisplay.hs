0b61edbf29f81b2752f485337208990db98af80f
5outh <bkovach13@gmail.com>
2014-02-22 18:58:46 -0500

    Remove some old useless junk and development scratch comments

--- src/DevTools/BoxDisplay.hs
+++ src/DevTools/BoxDisplay.hs
@@ -17,32 +17,10 @@ import qualified Data.Map as M
 makeLenses ''Room
 makeLenses ''Level
 
--- 3d
-showBoxed :: (Show a) => [(Vector3 Int, a)] -> String
-showBoxed vs = undefined
-  where bottoms = filter (\(Vector3 x y z, _) -> z == 0) vs
-        tops    = filter (\(Vector3 x y z, _) -> z == 1) vs
-        floor   = showInBox bottoms
-        ceiling = showInBox tops
--- 2d
--- Note rows will be 0..2
---showInBox :: Show a => [(Vector3 Int, a)] -> String
-showInBox vs = undefined
-  where longest = maximumBy (compare `on` (length . show . snd)) vs
-
-makeLength :: Int -> String -> String
-makeLength n str = let blanks = n - (length str) in str ++ (replicate blanks ' ')
-
-lookupBy :: (a -> a -> Bool) -> a -> [(a, b)] -> Maybe b
-lookupBy _  _ []          = Nothing
-lookupBy eq a ((b, x):xs) = if a `eq` b then Just x else lookupBy eq a xs
-
 -- | Show levels with demolished walls, etc.
 -- | Normal Room: --
 -- |             |  |
 -- |              --
--- | Up, Down, East, West
--- | ceilHole, floorHole, wallE, wallW
 showLevel :: Level -> String
 showLevel lvl = unlines ( map showRoomAll $ reverse $ transpose $ threes $ rms )
   where rms = M.toList $ lvl^.rooms
