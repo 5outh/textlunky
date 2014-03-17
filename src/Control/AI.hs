c633f5230e29f7e5ae69c905c3d6d353a6533f83
Benjamin Kovach <macbook@South.local>
2014-03-17 17:36:14 -0400

    Start AI module

--- src/Control/AI.hs
+++ src/Control/AI.hs
@@ -1,7 +1,38 @@
+{-# LANGUAGE DeriveFunctor #-}
 module Control.AI(
 ) where
 
 import Data.Vectors
+import Data.Direction
+import Control.Monad
 import Control.Monad.Free
 
-type AI cmd = Free cmd ()
+data AIAction a = 
+    Move Direction a
+  | Attack (Vector3 Int) Int a
+  | Explode (Vector3 Int) a
+    deriving (Functor, Eq)
+
+
+type AI = Free AIAction
+
+moveAI :: Direction -> AI ()
+moveAI d = liftF ( Move d () )
+
+attackAI :: Vector3 Int -> Int -> AI ()
+attackAI v dmg = liftF ( Attack v dmg () )
+
+explodeAI :: Vector3 Int -> AI ()
+explodeAI v = liftF ( Explode v () )
+
+moveDirs :: [Direction] -> AI ()
+moveDirs = forever . mapM_ (replicateM 3 . moveAI)
+
+moveEW :: AI ()
+moveEW = moveDirs [E, W]
+
+moveNS :: AI ()
+moveNS = moveDirs [N, S]
+
+moveUD :: AI ()
+moveUD = moveDirs [U, D]
