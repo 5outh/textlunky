04b9b0a75c5d775e51e142f351357a674397c391
Benjamin Kovach <macbook@s172-20-153-h131.paws.uga.edu>
2014-03-18 16:08:07 -0400

    Small AI update and some Lens help

--- src/Control/AI.hs
+++ src/Control/AI.hs
@@ -10,13 +10,21 @@ import Types.Synonyms hiding (Free)
 import Control.Monad
 import Control.Monad.Free
 
+-- [(1,2),(3,4),(1,5)] & traverse.filtered (has (_1.only 1))._2 %~ succ
+-- [(1,2),(3,4),(1,6)]
+-- modifyAtV :: (Eq v) => v -> (a -> a) -> [(v, a)] -> [(v, a)]
+-- modifyAtV v f = traverse.filtered (has (_1.only v))._2 %~ f
+-- [(100,2),(3,4)] & traverse.itraversed.Lens.indices (> 40) *~ 10
+-- [(100,2),(3,4)] & traverse.itraversed.Lens.index 100 *~ 10
+-- MUCH more efficient (for maps)
+-- M.fromList [(1,2)] & ix 1 *~ 10
+
 data AIAction a = 
     Move Direction a
   | Attack (Vector3 Int) Int a
   | Explode (Vector3 Int) a
   | End
     deriving (Functor, Eq)
-
 
 type AI = Free AIAction
 type AIUpdater a = AI () -> a -> a
@@ -48,6 +56,13 @@ moveUD = moveDirs [U, D]
 
 processAction :: AIProcess
 processAction (Free (Move _ a))       gs = processAction a gs
-processAction (Free (Attack v dmg a)) gs = processAction a gs
-processAction (Free (Explode v a))    gs = processAction a gs
+processAction (Free (Attack v dmg a)) gs = 
+  processAction a (attackAtWithDmg v dmg gs)
+processAction (Free (Explode v a))    gs = processAction a (explodeAt v gs)
 processAction (Free End)              gs = gs
+
+explodeAt :: Vector3 Int -> GameState -> GameState
+explodeAt v gs = gs
+
+attackAtWithDmg :: Vector3 Int -> Int -> GameState -> GameState
+attackAtWithDmg v dmg gs = gs
