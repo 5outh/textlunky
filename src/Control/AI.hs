679a05d8e6623eac4044e74ab4765b8598643c65
Benjamin Kovach <macbook@s172-20-84-h192.paws.uga.edu>
2014-03-18 13:45:51 -0400

    Small update to AI

--- src/Control/AI.hs
+++ src/Control/AI.hs
@@ -4,6 +4,9 @@ module Control.AI(
 
 import Data.Vectors
 import Data.Direction
+import Types.GameState
+import Types.Entity
+import Types.Synonyms hiding (Free)
 import Control.Monad
 import Control.Monad.Free
 
@@ -11,10 +14,16 @@ data AIAction a =
     Move Direction a
   | Attack (Vector3 Int) Int a
   | Explode (Vector3 Int) a
+  | End
     deriving (Functor, Eq)
 
 
 type AI = Free AIAction
+type AIUpdater a = AI () -> a -> a
+-- Update process for the entire game state
+type AIProcess  = AIUpdater GameState
+-- Update process for the entity in question
+type AIModifier = AIUpdater Entity
 
 moveAI :: Direction -> AI ()
 moveAI d = liftF ( Move d () )
@@ -36,3 +45,9 @@ moveNS = moveDirs [N, S]
 
 moveUD :: AI ()
 moveUD = moveDirs [U, D]
+
+processAction :: AIProcess
+processAction (Free (Move _ a))       gs = processAction a gs
+processAction (Free (Attack v dmg a)) gs = processAction a gs
+processAction (Free (Explode v a))    gs = processAction a gs
+processAction (Free End)              gs = gs
